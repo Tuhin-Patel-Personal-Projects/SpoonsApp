096d3db1ebe14384ae6a7cf3ecbd3d9e107fdea0
Tuhin-Patel <pateltuhin014@gmail.com>
2022-05-13 14:44:17 -0400

    Added functionality to task list view, still working on getting data to save

--- SpoonsPrototype/TaskViewController.swift
+++ SpoonsPrototype/TaskViewController.swift
@@ -9,16 +9,67 @@ import UIKit
 
 class TaskViewController: UITableViewController {
     var listName: String?
+    var taskList = [String]()
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addItem))
         
         if let name = listName {
             self.title = name
         }
 
-        // Do any additional setup after loading the view.
+       
     }
     
+    // How many cells are needed
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskList.count
+    }
+    
+    // How man
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Task", for: indexPath)
+        cell.textLabel?.text = taskList[indexPath.row] // Add the task name to the cell
+        return cell
+    }
+    
+    // Add new task to the main list
+    @objc func addItem() {
+        let ac = UIAlertController(title: "Enter new task", message: nil, preferredStyle: .alert)
+        ac.addTextField() // User enters answer here
+        
+        let submitTask = UIAlertAction(title: "Submit", style: .default) { // Trailing closure syntax
+        
+            
+            // Specifies input into closure, use weak so that the closure does not caputure it strongly
+            // Avoids strong reference cycle that retains memory for a long time
+            [weak self, weak ac] action  in
+            guard let newTask = ac?.textFields?[0].text else {return}
+            self?.taskList.append(newTask) // Add the new task to the list
+            self?.tableView.reloadData() // Reload the view
+            
+        }
+        ac.addAction(submitTask)
+        
+        present(ac, animated: true)
+    }
+    
+    // Enables deleting
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            taskList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath] , with: UITableView.RowAnimation.automatic )
+        }
+    }
+    
+    // Enables swapping
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        taskList.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+    }
 
     /*
     // MARK: - Navigation
@@ -30,4 +81,5 @@ class TaskViewController: UITableViewController {
     }
     */
 
+    
 }
